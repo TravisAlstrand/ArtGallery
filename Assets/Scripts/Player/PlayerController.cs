1dcc778e3abfafb570dd46ae2e4e9e2d3a5796b2
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2025-01-16 16:46:09 -0800

    directional animations added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float lastX = 0f;
+    private float lastY = -1f;
+
     private PlayerInput playerInput;
     private FrameInput frameInput;
     private PlayerMovement playerMovement;
+    private Animator animator;
 
     private void Awake() {
         playerInput = GetComponent<PlayerInput>();
         playerMovement = GetComponent<PlayerMovement>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update() {
         GatherInput();
-        playerMovement.SetCurrentDirection(frameInput.Move);
+        HandleMovement();
     }
 
     private void GatherInput() {
         frameInput = playerInput.FrameInput;
+    }
+
+    private void HandleMovement() {
+        playerMovement.SetCurrentDirection(frameInput.Move);
+        if (frameInput.Move != Vector2.zero) {
+            animator.SetFloat("xDir", frameInput.Move.x);
+            animator.SetFloat("yDir", frameInput.Move.y);
+            lastX = frameInput.Move.x;
+            lastY = frameInput.Move.y;
+        } else {
+            animator.SetFloat("xDir", lastX);
+            animator.SetFloat("yDir", lastY);
+        }
     }
 }
